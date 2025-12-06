8d4a179f45db6263d668d3f1c0fac184fc07f85d
Byrne Hollander <bhollander823@gmail.com>
2025-12-05 20:48:58 -0500

    Data update

--- src/shared.js
+++ src/shared.js
@@ -23,7 +23,7 @@ export const FORMATS = {
   },
 };
 
-export const SELECTED_FORMAT = FORMATS.PREMIER_DRAFT;
+export const SELECTED_FORMAT = FORMATS.SEALED;
 
 // When true, preprocessing will fetch data from all formats and calculate weighted averages
 // When false, preprocessing will only fetch data from SELECTED_FORMAT
